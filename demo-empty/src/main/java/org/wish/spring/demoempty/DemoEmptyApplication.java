c87c8e1bb4bb7f8e4acaf5c82a49e41092dcf593
Wish <wishxavier@gmail.com>
2023-02-23 21:19:53 +0800

    First Spring project. Do nothing.

---
+++ demo-empty/src/main/java/org/wish/spring/demoempty/DemoEmptyApplication.java
@@ -0,0 +1,13 @@
+package org.wish.spring.demoempty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEmptyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEmptyApplication.class, args);
+    }
+
+}
---
+++ demo-empty/src/test/java/org/wish/spring/demoempty/DemoEmptyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wish.spring.demoempty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEmptyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
